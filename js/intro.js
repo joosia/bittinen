37c25b8060398047aefeb86b65bea9d184d9971f
joosia <30998472+joosia@users.noreply.github.com>
2018-01-31 00:38:47 +0200

    - Javascript re-written in jquery - Added drop-shadow to browser picture instead of using css box-shadow - issues: url and puzzle alignment

--- js/intro.js
+++ js/intro.js
@@ -1,81 +1,65 @@
-var url = document.getElementById('url'); // URL-bar
-var browser = document.getElementById('browser'); // Browser
-var intro = document.getElementById('intro'); // Intro container
-var form = document.getElementById('form'); // URL form
-// Start puzzle element
-var start = document.getElementById('startPuzzle')
-// Puzzle solved element
-var solved = document.getElementById('solved');
+$(document).ready(function() {
+  // Click browser to focus URL-bar
+  $("#browser").click(function(){
+     $(".url").focus();
+   });
 
-// Click browser to focus URL-bar
-browser.onclick = function() {url.focus();};
+  // Enlarge browser & text on focus & the opposite on blur
+  $(".url").on("focus blur", function(){
+    $(".browserContainer").toggleClass("bcLarge");
+    $(this).toggleClass("urlLarge ");
+  });
 
-// Increases size of the text and the browser when url is clicked
-url.onfocus = function() {
-   browser.setAttribute("style", "width: 86%; margin: 0 7% 0 7%;");
-   url.setAttribute("style", "font-size: 16px; top:15.5%; margin: 0 0 0 21%;");
-};
-//Normalize sizes when clicked outside the url
-url.onblur = function() {
-      browser.setAttribute("style", "width: 80%; margin: 0 10% 0 10%;");
-      url.setAttribute("style", "font-size: 14px; top: 22%;");
-};
+  // jQuery: Smooth Scroll on click (link-elements)
+  var $root = $('html, body'); // cache for better performance?
+  $('a').on("click", function() {
+     $('html, body').animate( {
+        scrollTop:$($(this).attr('href')).offset().top
+     },700);
+     return false;
+  });
 
-// jQuery: Smooth Scroll on click (link-elements)
-var $root = $('html, body'); // cache for better performance?
-$('a').click(function() {
-   $('html, body').animate( {
-      scrollTop:$($(this).attr('href')).offset().top
-   },700);
-   return false;
-});
+  // // Returns height of browser viewport
+  // var h = $( window ).height();
+  // console.log(h);
 
-//Scroll on arrow keys
-window.addEventListener("keydown", function (event) {
-   var h = window.innerHeight;
-   if (event.defaultPrevented) {
-      return; // Do nothing if the event was already processed
-   }
-   switch (event.key) {
-      case "ArrowDown":
-         scrollBy(0, h)
-            break;
-      case "ArrowUp":
-         scrollBy(0, -h)
-            break;
-      // case "ArrowLeft":
-      //   // Do something for "left arrow" key press.
-      //   break;
-      // case "ArrowRight":
-      //   // Do something for "right arrow" key press.
-      //   break;
-      // case "Enter":
-      //   // Do something for "enter" or "return" key press.
-      //   break;
-      // case "Escape":
-      //   // Do something for "esc" key press.
-      //    break;
-    default:
-      return; // Quit when this doesn't handle the key event.
-   }
-  // Cancel the default action to avoid it being handled twice
-  event.preventDefault();
-}, true);
+  // Smooth Scroll on arrow keys
+  $(document).keydown(function(event) {
 
-// jQuery: PUZZLE SETTINGS
-start.onclick = function() {;
-   browser.setAttribute("style", "padding-bottom:0%;");
-   // intro.setAttribute("style", "max-height: 100%; overflow: auto;");
-   puzzlePic.setAttribute("style","display: block;");
-   $('#puzzlePic').snapPuzzle({
+    switch (event.which) {
+      case 38: // Arrow Up
+        $("html, body").animate({
+          scrollTop: "-=738",
+        }, 700);
+
+        break;
+      case 40: // Arrow Down
+        $("html, body").animate({
+          scrollTop: "+=738",
+        }, 700);
+        break;
+      default: return; // Exit this handler for other keys
+    }
+    event.preventDefault();
+  });
+
+  // Start the puzzle
+  $("#startPuzzle").click(function(){
+    // Show the puzzle picture
+    $("#puzzlePic").css("display","block");
+    // Prevents scrolling
+    // $("body").css("overflow","hidden"); // Disables page scroll
+    $(".browserContainer").css("top","15%");
+    // Puzzle Settings
+    $("#puzzlePic").snapPuzzle({
       rows: 5,
       columns: 5,
-      containment: '.browser',
+      containment: '#intro',
       pile: '.pile',
       onComplete: function(){
-         $('#puzzlePic').fadeOut(150).fadeIn();
-         intro.setAttribute("style", "height: 100%; overflow: hidden;");
-         solved.setAttribute("style", "display: block;");
-      }
-   });
-};
+        $("#puzzlePic").fadeOut(150).fadeIn();
+        $("#solved.css").css("display","block");
+       }
+    });
+  });
+});
