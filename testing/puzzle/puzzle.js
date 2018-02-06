b790e8ed13f0de71351db3419b0c5c37749a1446
joosia <30998472+joosia@users.noreply.github.com>
2018-02-06 22:58:24 +0200

    Added simple puzzle to testing folder

---
+++ testing/puzzle/puzzle.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+  $("body").click(function(){
+      $("#overlay").toggle("slow");
+  })
+
+  // Make .draggable elements draggable and stackable
+  $(".draggable").draggable({stack:"img"});
+
+  // Add droppable target
+  $('.slots img[data-index]').droppable({
+
+    accept: function(piece) {
+      var dragIndex, dropIndex;
+
+      dropIndex = $(piece).attr("data-index")
+      dragIndex = $(this).attr("data-index")
+      // if the data-index matches accept drop
+      return dropIndex == dragIndex;
+    },
+    hoverClass : "drop-hover",
+    drop: function(event, ui) {
+      $(ui.draggable).fadeTo("slow", 0.0);
+      $(this).css("filter","grayscale(0)");
+    }
+  });
+
+  // Remove element
+  function removeEl() {
+    $(this).remove();
+  }
+  // 
+  // if( $(".pieces").has("img") ) {
+  // }
+  // else {
+  //   $("#overlay").toggle("slow");
+  // }
+
+}); // document ready
