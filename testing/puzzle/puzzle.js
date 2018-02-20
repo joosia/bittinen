55dd447b8273628fb8b473737b0d91d2e3edef8a
joosia <30998472+joosia@users.noreply.github.com>
2018-02-20 11:59:28 +0200

    testing puzzle updates

--- testing/puzzle/puzzle.js
+++ testing/puzzle/puzzle.js
@@ -1,39 +1,42 @@
 $(document).ready(function() {
 
-  $("body").click(function(){
+   $("body").click(function(){
       $("#overlay").toggle("slow");
-  })
+   })
 
-  // Make .draggable elements draggable and stackable
-  $(".draggable").draggable({stack:"img"});
+   // Make .draggable elements draggable and stackable
+   $(".draggable").draggable({stack:"img"});
 
-  // Add droppable target
-  $('.slots img[data-index]').droppable({
+   // Add droppable target
+   $('.puzzleImg img[data-index]').droppable({
 
-    accept: function(piece) {
-      var dragIndex, dropIndex;
+      accept: function(piece) {
+         var dragIndex, dropIndex;
 
-      dropIndex = $(piece).attr("data-index")
-      dragIndex = $(this).attr("data-index")
-      // if the data-index matches accept drop
-      return dropIndex == dragIndex;
-    },
-    hoverClass : "drop-hover",
-    drop: function(event, ui) {
-      $(ui.draggable).fadeTo("slow", 0.0);
-      $(this).css("filter","grayscale(0)");
-    }
-  });
+         dropIndex = $(piece).attr("data-index")
+         dragIndex = $(this).attr("data-index")
+         // if the data-index matches accept drop
+         return dropIndex == dragIndex;
+      },
+      hoverClass : "drop-hover",
+      drop: function(event, ui) {
+         $(ui.draggable).fadeTo("slow", 0.0);
+         $(this).css("filter","grayscale(0)");
+      }
+   });
 
-  // Remove element
-  function removeEl() {
-    $(this).remove();
-  }
-  // 
-  // if( $(".pieces").has("img") ) {
-  // }
-  // else {
-  //   $("#overlay").toggle("slow");
-  // }
+   // Remove element
+   function removeEl() {
+      $(this).remove();
+   }
+
+   $(".slot").css("display", "none");
+
+   //
+   // if( $(".pieces").has("img") ) {
+   // }
+   // else {
+   //   $("#overlay").toggle("slow");
+   // }
 
 }); // document ready
